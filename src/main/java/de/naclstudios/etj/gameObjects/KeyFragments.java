704acc6a3ebe73696f56d5a9165af4347d09f516
edgelord <malte.dostal@gmail.com>
2018-08-13 20:37:05 +0200

    Some work again

--- src/main/java/de/naclstudios/etj/gameObjects/KeyFragments.java
+++ src/main/java/de/naclstudios/etj/gameObjects/KeyFragments.java
@@ -1,23 +1,35 @@
 package de.naclstudios.etj.gameObjects;
 
 import de.edgelord.sjgl.core.event.CollisionEvent;
+import de.edgelord.sjgl.factory.ImageFactory;
 import de.edgelord.sjgl.gameobject.GameObject;
+import de.edgelord.sjgl.gameobject.components.rendering.ImageRender;
 import de.edgelord.sjgl.location.Coordinates;
+import de.edgelord.sjgl.resource.InnerResource;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class KeyFragments extends GameObject {
 
+    private BufferedImage image = new ImageFactory(new InnerResource()).getOptimizedImageResource("pictures/key.png");
+    private ImageRender render = new ImageRender(this, "imageRender", image);
+
     public KeyFragments(Coordinates coordinates) {
-        super(coordinates, 0, 0, "");
+        super(coordinates, 201, 108, "de.naclstudios.etj.gameObjects.keyFragment");
     }
 
     public void initialize() {
 
     }
 
-    public void onCollision(CollisionEvent collisionEvent) {
+    public void onCollision(CollisionEvent e) {
 
+        if (e.getRoot().getTag().equals("de.naclstudios.etj.gameObject.player")){
+            Player player = (Player) e.getRoot();
+
+            player.setCollectedKeyFragments(player.getCollectedKeyFragments() + 1);
+        }
     }
 
     public void onFixedTick() {
