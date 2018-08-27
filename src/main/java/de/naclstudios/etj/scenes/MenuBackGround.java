2b377ef668eeec63afc85d1d577a333e92c988bf
edgelord <malte.dostal@gmail.com>
2018-08-27 19:15:47 +0200

    Use the latest version of salty-engine and suit to it

--- src/main/java/de/naclstudios/etj/scenes/MenuBackGround.java
+++ src/main/java/de/naclstudios/etj/scenes/MenuBackGround.java
@@ -1,15 +1,19 @@
 package de.naclstudios.etj.scenes;
 
 import de.edgelord.saltyengine.factory.ImageFactory;
-import de.edgelord.saltyengine.gameobject.DrawingRoutin;
+import de.edgelord.saltyengine.gameobject.DrawingRoutine;
 import de.edgelord.saltyengine.resource.InnerResource;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class MenuBackGround implements DrawingRoutin {
+public class MenuBackGround extends DrawingRoutine {
 
     private BufferedImage image = new ImageFactory(new InnerResource()).getOptimizedImageResource("pictures/grey.png");
+
+    public MenuBackGround() {
+        super(DrawingPosition.BEFORE_GAMEOBJECTS);
+    }
 
     public void draw(Graphics2D graphics2D) {
 
